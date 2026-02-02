ea13d9b9ffe1778a452f5ea5765e0e751c4344b5
Marta2901 <martaenriquezfigueroa@gmail.com>
2026-02-02 09:15:36 +0100

    Editado archivo __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'OpenWeatherMAp Integration',
+    'version': '1.0',
+    'summary': 'Integración con la API de OpenWeatherMap',
+    'description': 'Obtención de datos meteorológicos en tiempo real',
+    'author': 'Marta Enríquez Figueroa',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/weather_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'icon':['/openweathermao_odoo/static/description/icon57.png'],
+}
